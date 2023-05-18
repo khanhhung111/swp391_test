c092d6f719d29b040ca2d59ab2d404491f9d5969
khanhhung111 <hungtvkde160432@fpt.edu.vn>
2023-05-18 19:58:08 +0700

    update_v2

--- src/java/model/Account.java
+++ src/java/model/Account.java
@@ -9,14 +9,14 @@ package model;
  * @author Dell
  */
 public class Account {
-    private String userID, userName, password, role;
+    private String userID, email, password, role;
 
     public Account() {
     }
 
-    public Account(String userID, String userName, String password, String role) {
+    public Account(String userID, String email, String password, String role) {
         this.userID = userID;
-        this.userName = userName;
+        this.email = email;
         this.password = password;
         this.role = role;
     }
@@ -29,12 +29,12 @@ public class Account {
         this.userID = userID;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
